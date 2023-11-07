cfde3d0faee4af9fba24dc33f33a3dcd8be19b83
VAL3NTINE05 <ksw00014@mix.wvu.edu>
2023-11-06 19:19:58 -0500

    added data caching to overcome API rate limiting challenges

--- src/WeatherForecastApp.java
+++ src/WeatherForecastApp.java
@@ -18,12 +18,16 @@ import java.util.Calendar;
 import java.awt.GridLayout;
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.Map;
+import java.util.HashMap;
 
 public class WeatherForecastApp {
+    private static Map<String, WeatherData> dataCache = new HashMap<>();
+
     public static void main(String[] args) {
         Properties properties = new Properties();
         try {
-            FileInputStream input = new FileInputStream("C:\\Users\\kaush\\IdeaProjects\\WeatherApp\\src\\config.properties");
+            FileInputStream input = new FileInputStream("src\\config.properties");
             properties.load(input);
         } catch (Exception e) {
             e.printStackTrace();
@@ -34,104 +38,155 @@ public class WeatherForecastApp {
         String apiKey = properties.getProperty("api_key");
 
         try {
-            // Prompt the user for a city
             String cityName = JOptionPane.showInputDialog("Enter the name of the city:");
 
             if (cityName != null && !cityName.isEmpty()) {
-                // Get current weather data
-                URL currentWeatherURL = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey);
-                HttpURLConnection currentWeatherConn = (HttpURLConnection) currentWeatherURL.openConnection();
-                currentWeatherConn.setRequestMethod("GET");
-                BufferedReader currentWeatherReader = new BufferedReader(new InputStreamReader(currentWeatherConn.getInputStream()));
-                String currentWeatherLine;
-                StringBuilder currentWeatherResult = new StringBuilder();
-                while ((currentWeatherLine = currentWeatherReader.readLine()) != null) {
-                    currentWeatherResult.append(currentWeatherLine);
-                }
-                currentWeatherReader.close();
+                WeatherData cachedData = dataCache.get(cityName);
 
-                // Parse current weather data
-                JSONObject currentWeatherJson = new JSONObject(currentWeatherResult.toString());
-                double tempKelvin = currentWeatherJson.getJSONObject("main").getDouble("temp");
-                String condition = currentWeatherJson.getJSONArray("weather").getJSONObject(0).getString("description");
-
-                // Convert temperature to Fahrenheit
-                double tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
-
-                // Get forecast data
-                URL forecastURL = new URL("http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=" + apiKey);
-                HttpURLConnection forecastConn = (HttpURLConnection) forecastURL.openConnection();
-                forecastConn.setRequestMethod("GET");
-                BufferedReader forecastReader = new BufferedReader(new InputStreamReader(forecastConn.getInputStream()));
-                String forecastLine;
-                StringBuilder forecastResult = new StringBuilder();
-                while ((forecastLine = forecastReader.readLine()) != null) {
-                    forecastResult.append(forecastLine);
-                }
-                forecastReader.close();
-
-                // Parse forecast data
-                JSONObject forecastJson = new JSONObject(forecastResult.toString());
-                JSONArray forecastList = forecastJson.getJSONArray("list");
-
-                // Create GUI
-                JFrame frame = new JFrame("Weather Forecast");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLayout(new GridLayout(1, 1)); // 1 row, 1 column
-
-                // Create and set the seasonal background image to cover the entire window
-                Calendar calendar = Calendar.getInstance();
-                int month = calendar.get(Calendar.MONTH);
-                String imagePath;
-                if (month >= Calendar.MARCH && month <= Calendar.MAY) {
-                    imagePath = "C:\\Users\\kaush\\IdeaProjects\\WeatherApp\\images\\spring.jpg";
-                } else if (month >= Calendar.JUNE && month <= Calendar.AUGUST) {
-                    imagePath = "C:\\Users\\kaush\\IdeaProjects\\WeatherApp\\images\\summer.jpg";
-                } else if (month >= Calendar.SEPTEMBER && month <= Calendar.NOVEMBER) {
-                    imagePath = "C:\\Users\\kaush\\IdeaProjects\\WeatherApp\\images\\fall.jpg";
+                if (cachedData != null && isCacheValid(cachedData)) {
+                    displayWeatherData(cachedData);
                 } else {
-                    imagePath = "C:\\Users\\kaush\\IdeaProjects\\WeatherApp\\images\\winter.jpg";
+                    WeatherData newData = fetchWeatherData(apiKey, cityName);
+                    if (newData != null) {
+                        dataCache.put(cityName, newData);
+                        displayWeatherData(newData);
+                    }
                 }
-                BufferedImage image = ImageIO.read(new File(imagePath));
-                ImageIcon backgroundIcon = new ImageIcon(image);
-                JLabel backgroundLabel = new JLabel(backgroundIcon);
-
-                // Create and set the current weather and forecast information
-                JPanel infoPanel = new JPanel(new GridLayout(2, 1));
-                double tempFahrenheitRounded = Math.round(tempFahrenheit);
-                JTextArea currentWeatherText = new JTextArea("Current Temperature: " + (int) tempFahrenheitRounded + " °F\nCondition: " + condition);
-
-                JTextArea forecastTextArea = new JTextArea("5-Day Forecast:\n");
-                for (int i = 0; i < forecastList.length(); i += 8) { // Display every 24 hours (8 items per day)
-                    JSONObject forecastData = forecastList.getJSONObject(i);
-                    long timestamp = forecastData.getLong("dt");
-                    double forecastTemp = forecastData.getJSONObject("main").getDouble("temp");
-                    String forecastCondition = forecastData.getJSONArray("weather").getJSONObject(0).getString("description");
-
-                    // Convert temperature to Fahrenheit
-                    double forecastTempFahrenheit = (forecastTemp - 273.15) * 9 / 5 + 32;
-
-                    // Format date from timestamp (you can use SimpleDateFormat for better formatting)
-                    String forecastDate = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-                            .format(new java.util.Date(timestamp * 1000));
-
-                    forecastTextArea.append("\n" + forecastDate + ": " + (int) forecastTempFahrenheit + " °F, " + forecastCondition);
-                }
-
-                infoPanel.add(currentWeatherText);
-                infoPanel.add(new JScrollPane(forecastTextArea));
-
-                // Add the background label and info panel to the frame
-                frame.add(backgroundLabel);
-                frame.add(infoPanel);
-
-                frame.setSize(800, 600);
-                frame.setVisible(true);
             } else {
                 JOptionPane.showMessageDialog(null, "City name cannot be empty.");
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isCacheValid(WeatherData data) {
+        long currentTime = System.currentTimeMillis() / 1000;
+        return (currentTime - data.getTimestamp()) <= 3600; // Cache data for 1 hour
+    }
+
+    private static WeatherData fetchWeatherData(String apiKey, String cityName) {
+        try {
+            URL currentWeatherURL = new URL("http://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=" + apiKey);
+            HttpURLConnection currentWeatherConn = (HttpURLConnection) currentWeatherURL.openConnection();
+            currentWeatherConn.setRequestMethod("GET");
+            BufferedReader currentWeatherReader = new BufferedReader(new InputStreamReader(currentWeatherConn.getInputStream()));
+            String currentWeatherLine;
+            StringBuilder currentWeatherResult = new StringBuilder();
+            while ((currentWeatherLine = currentWeatherReader.readLine()) != null) {
+                currentWeatherResult.append(currentWeatherLine);
+            }
+            currentWeatherReader.close();
+
+            JSONObject currentWeatherJson = new JSONObject(currentWeatherResult.toString());
+            double tempKelvin = currentWeatherJson.getJSONObject("main").getDouble("temp");
+            String condition = currentWeatherJson.getJSONArray("weather").getJSONObject(0).getString("description");
+
+            double tempFahrenheit = (tempKelvin - 273.15) * 9/5 + 32;
+
+            URL forecastURL = new URL("http://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&APPID=" + apiKey);
+            HttpURLConnection forecastConn = (HttpURLConnection) forecastURL.openConnection();
+            forecastConn.setRequestMethod("GET");
+            BufferedReader forecastReader = new BufferedReader(new InputStreamReader(forecastConn.getInputStream()));
+            String forecastLine;
+            StringBuilder forecastResult = new StringBuilder();
+            while ((forecastLine = forecastReader.readLine()) != null) {
+                forecastResult.append(forecastLine);
+            }
+            forecastReader.close();
+
+            JSONObject forecastJson = new JSONObject(forecastResult.toString());
+
+            WeatherData weatherData = new WeatherData(tempFahrenheit, condition, forecastJson, System.currentTimeMillis() / 1000);
+
+            return weatherData;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static void displayWeatherData(WeatherData data) {
+        JFrame frame = new JFrame("Weather Forecast");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new GridLayout(1, 1));
+
+        Calendar calendar = Calendar.getInstance();
+        int month = calendar.get(Calendar.MONTH);
+        String imagePath;
+        if (month >= Calendar.MARCH && month <= Calendar.MAY) {
+            imagePath = "images\\spring.jpg";
+        } else if (month >= Calendar.JUNE && month <= Calendar.AUGUST) {
+            imagePath = "images\\summer.jpg";
+        } else if (month >= Calendar.SEPTEMBER && month <= Calendar.NOVEMBER) {
+            imagePath = "images\\fall.jpg";
+        } else {
+            imagePath = "images\\winter.jpg";
+        }
+        try {
+            BufferedImage image = ImageIO.read(new File(imagePath));
+            ImageIcon backgroundIcon = new ImageIcon(image);
+            JLabel backgroundLabel = new JLabel(backgroundIcon);
+
+            JPanel infoPanel = new JPanel(new GridLayout(2, 1));
+            double tempFahrenheitRounded = Math.round(data.getTemperature());
+            JTextArea currentWeatherText = new JTextArea("Current Temperature: " + (int) tempFahrenheitRounded + " °F\nCondition: " + data.getCondition());
+
+            JTextArea forecastTextArea = new JTextArea("5-Day Forecast:\n");
+            for (int i = 0; i < data.getForecastList().length(); i += 8) {
+                JSONObject forecastData = data.getForecastList().getJSONObject(i);
+                long timestamp = forecastData.getLong("dt");
+                double forecastTemp = forecastData.getJSONObject("main").getDouble("temp");
+                String forecastCondition = forecastData.getJSONArray("weather").getJSONObject(0).getString("description");
+
+                double forecastTempFahrenheit = (forecastTemp - 273.15) * 9 / 5 + 32;
+
+                String forecastDate = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
+                        .format(new java.util.Date(timestamp * 1000));
+
+                forecastTextArea.append("\n" + forecastDate + ": " + (int) forecastTempFahrenheit + " °F, " + forecastCondition);
+            }
+
+            infoPanel.add(currentWeatherText);
+            infoPanel.add(new JScrollPane(forecastTextArea));
+
+            frame.add(backgroundLabel);
+            frame.add(infoPanel);
+
+            frame.setSize(800, 600);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class WeatherData {
+    private double temperature;
+    private String condition;
+    private JSONArray forecastList;
+    private long timestamp;
+
+    public WeatherData(double temperature, String condition, JSONObject forecastJson, long timestamp) {
+        this.temperature = temperature;
+        this.condition = condition;
+        this.forecastList = forecastJson.getJSONArray("list");
+        this.timestamp = timestamp;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public JSONArray getForecastList() {
+        return forecastList;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
     }
 }
